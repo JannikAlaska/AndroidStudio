f55048be4369cd1844ae4ca7a36af62b95102ec1
Berthold <Berthold@Ilsenburg.local>
2020-12-08 10:39:40 +0100

    Initial commit

---
+++ app/src/main/java/de/berthold_ilsenburg/berthold/intro/MainActivity.java
@@ -0,0 +1,19 @@
+package de.berthold_ilsenburg.berthold.intro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickButton (View view) {
+        Log.i("Info", "Button wurde geklickt");
+    }
+}
